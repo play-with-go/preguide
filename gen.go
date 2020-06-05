d1ec8dbff76db1282fb40bb34e618a70c0efdb2f
Paul Jolly <paul@myitcv.io>
2020-06-05 16:27:53 +0100

    Consolidate CUE code gen away from root dir

--- gen.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate go run cuelang.org/go/cmd/cue cmd -t workflowsDir=./.github/workflows gengithub ./internal/ci
-//go:generate go run cuelang.org/go/cmd/cue cmd -t scriptsDir=./_scripts genenv ./internal/ci
---
+++ internal/ci/gen.go
@@ -0,0 +1,4 @@
+package ci
+
+//go:generate go run cuelang.org/go/cmd/cue cmd gengithub .
+//go:generate go run cuelang.org/go/cmd/cue cmd genenv .
