2a2b01d2b6796ec15c63458e50e0b5b86a5eb276
Paul Jolly <paul@myitcv.io>
2020-09-07 11:40:03 +0100

    general: add license

--- internal/embed/embed.go
+++ internal/embed/embed.go
@@ -1,5 +1,9 @@
+// Copyright 2020 The play-with-go.dev Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
 package embed
 
-//go:generate go run github.com/jteeuwen/go-bindata/go-bindata -pkg embed -o gen_bindata.go -nocompress -prefix ../../ -ignore ".(go|mod|sum|vimrc)$" ../../ ../../out ../../cue.mod
+//go:generate go run github.com/jteeuwen/go-bindata/go-bindata -pkg embed -o gen_bindata.go -nocompress -prefix ../../ -ignore "LICENSE|.(go|mod|sum|vimrc)$" ../../ ../../out ../../cue.mod
 //go:generate go run cuelang.org/go/cmd/cue cmd -t file=gen_bindata.go prefix
 //go:generate gofmt -w -s gen_bindata.go
