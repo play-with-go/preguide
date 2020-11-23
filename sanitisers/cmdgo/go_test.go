d71a1f64a6fa85c5553f15e209e9c9039b0dfcae
Paul Jolly <paul@myitcv.io>
2020-11-23 10:30:50 +0000

    cmd/preguide: sanitise go test output for comparison (#168)
    This leaves "real" values in place, which for benchmarks is important

--- sanitisers/cmdgo/go_test.go
+++ sanitisers/cmdgo/go_test.go
@@ -21,47 +21,6 @@ func TestSanitiseOutputGoTest(t *testing.T) {
 		want string
 	}{
 		{
-			san: sanitiseGoTest{},
-			in: `=== RUN   TestGood
-=== RUN   TestGood/something
-ok
---- PASS: TestGood (0.00s)
-    --- PASS: TestGood/something (0.00s)
-=== RUN   TestBad
-=== RUN   TestBad/something
-    TestBad/something: main_test.go:16: bad
---- FAIL: TestBad (0.00s)
-    --- FAIL: TestBad/something (0.00s)
-FAIL
-FAIL	mod.go	0.002s
-=== RUN   TestP
-    TestP: p_test.go:6: =========
---- FAIL: TestP (0.00s)
-FAIL
-FAIL	 mod.go/p 	0.002s
-FAIL
-`,
-			want: `=== RUN   TestGood
-=== RUN   TestGood/something
-ok
---- PASS: TestGood (0.042s)
-    --- PASS: TestGood/something (0.042s)
-=== RUN   TestBad
-=== RUN   TestBad/something
-    TestBad/something: main_test.go:16: bad
---- FAIL: TestBad (0.042s)
-    --- FAIL: TestBad/something (0.042s)
-FAIL
-FAIL	mod.go	0.042s
-=== RUN   TestP
-    TestP: p_test.go:6: =========
---- FAIL: TestP (0.042s)
-FAIL
-FAIL	 mod.go/p 	0.042s
-FAIL
-`,
-		},
-		{
 			san: sanitiseGoGet{},
 			in: `go get example.com/private: module example.com/private: reading https://proxy.golang.org/example.com/private/@v/list: 410 Gone
 	server response:
@@ -165,10 +124,53 @@ GOGCCFLAGS="-fPIC -m64 -pthread -fmessage-length=0 -fdebug-prefix-map=/tmp/go-bu
 
 func TestSanitiseComparisonOutputGoGet(t *testing.T) {
 	testCases := []struct {
+		san  sanitisers.Sanitiser
 		in   string
 		want string
 	}{
 		{
+			san: sanitiseGoTest{},
+			in: `=== RUN   TestGood
+=== RUN   TestGood/something
+ok
+--- PASS: TestGood (0.00s)
+    --- PASS: TestGood/something (0.00s)
+=== RUN   TestBad
+=== RUN   TestBad/something
+    TestBad/something: main_test.go:16: bad
+--- FAIL: TestBad (0.00s)
+    --- FAIL: TestBad/something (0.00s)
+FAIL
+FAIL	mod.go	0.002s
+=== RUN   TestP
+    TestP: p_test.go:6: =========
+--- FAIL: TestP (0.00s)
+FAIL
+FAIL	 mod.go/p 	0.002s
+FAIL
+`,
+			want: `=== RUN   TestGood
+=== RUN   TestGood/something
+ok
+--- PASS: TestGood (N.NNs)
+    --- PASS: TestGood/something (N.NNs)
+=== RUN   TestBad
+=== RUN   TestBad/something
+    TestBad/something: main_test.go:16: bad
+--- FAIL: TestBad (N.NNs)
+    --- FAIL: TestBad/something (N.NNs)
+FAIL
+FAIL	mod.go	N.NNs
+=== RUN   TestP
+    TestP: p_test.go:6: =========
+--- FAIL: TestP (N.NNs)
+FAIL
+FAIL	 mod.go/p 	N.NNs
+FAIL
+`,
+		},
+		{
+			san: sanitiseGoGet{},
 			in: `go: downloading golang.org/x/tools v0.0.0-20201105220310-78b158585360
 go: found golang.org/x/tools/cmd/stringer in golang.org/x/tools v0.0.0-20201105220310-78b158585360
 go: downloading golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
@@ -183,8 +185,7 @@ go: found golang.org/x/tools/cmd/stringer in golang.org/x/tools v0.0.0-202011052
 	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("TestSanitiseComparisonOutputGoGet_%v", i), func(t *testing.T) {
-			var san sanitiseGoGet
-			got := san.ComparisonOutput(nil, tc.in)
+			got := tc.san.ComparisonOutput(nil, tc.in)
 			if got != tc.want {
 				t.Fatalf("failed to get sanitised output: %v", cmp.Diff(got, tc.want))
 			}
